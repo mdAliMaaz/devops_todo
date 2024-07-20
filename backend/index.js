8149c46556e0d4fcecae8c52679c7a34ff11d6b0
Maaz <mdmaazi2907@gmail.com>
2024-07-20 15:12:06 +0530

    bug fixed

--- backend/index.js
+++ backend/index.js
@@ -18,13 +18,12 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use('/todos', require('./routes/todos'));
 
-if (process.env.NODE_ENV === "production") {
+
 	app.use(express.static(path.join(__dirname,"..", "/frontend/dist")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
